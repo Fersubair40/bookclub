e0c7681d3fbe036a8f485dae53743291b5f1ea04
Temilayo Subair <olajnrsubair@gmail.com>
2021-06-17 09:00:17 +0100

    404 for when user is not found

--- app/controllers/api/v1/authenticate_controller.rb
+++ app/controllers/api/v1/authenticate_controller.rb
@@ -1,11 +1,14 @@
 class Api::V1::AuthenticateController < ApplicationController
   def create
     @user = User.find_by_username(user_params[:username])
+
     if @user&.authenticate(user_params[:password])
       render json: {
         id: @user.id,
         token: JsonWebToken.encode(user_id: @user.id)
       }, status: :ok
+    elsif !@user
+      head :not_found
     elsif @user.failed_attempts >= 3
       render json: {message: "Account has been locked come back in 30 minutes"}, status: :locked
     else
